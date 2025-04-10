5d300c9858eb94a8fa602463c8424f863b883f77
Suhas2Anumolu <137101618+Suhas2Anumolu@users.noreply.github.com>
2025-04-10 19:06:26 -0400

    Create Upgrade.java

---
+++ Upgrade.java
@@ -0,0 +1,17 @@
+public class Upgrade {
+    private String name;
+    private int cost;
+
+    public Upgrade(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
